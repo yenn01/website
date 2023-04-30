76c874f56a8dcfcdbb5ca8b937ace5e645bcdb4d
yenn01 <yenn.m17@gmail.com>
2023-05-01 03:06:01 +0800

    Added postcss

---
+++ postcss.config.cjs
@@ -0,0 +1,15 @@
+const autoprefixer = require('autoprefixer');
+
+// const config = {
+// 	plugins: {
+//         autoprefixer: {}
+//     }
+// };
+
+// module.exports = config;
+
+module.exports = {
+    plugins: {
+      autoprefixer: {}
+    }
+  }
